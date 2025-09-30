599a5386652bbd10bd2a7eeff90387b58530d121
Javi17-gib <luisvalverde1330@gmail.com>
2025-09-30 16:39:09 -0600

    Eliminar Registros

--- blog/app/Http/Controllers/UsersController.php
+++ blog/app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class UsersController extends Controller
 {
@@ -13,7 +15,27 @@ class UsersController extends Controller
         //dd($data);
         return view ("admin.users")->with('usuarios',$data);
     }
-    public function createUsers(){
-        dd("Si jala tu");
+    public function createUsers(Request $request){
+        //dd($request->email);
+        //reglas de validacion
+        $request->validate([
+            "name"=>'required|min:3',
+            "nickname"=>'required|min:3|unique:users,nickname',
+            "email"=>'required|email|unique:users,email',
+            "password"=>'required|min:4',
+            "password2"=>'required|min:4|same:password'
+        ]);
+
+        //GUARDAR REGISTROS
+        $user = new User();
+        $user->name=$request->name;
+        $user->nickname=$request->nickname;
+        $user->password=Hash::make($request->password);
+        $user->email=$request->email;
+        $user->img="default,jpg";
+        $user->save();
+        return redirect()
+            ->back()
+            ->with('success', "Usuario Insetado Correctamente");
     }
 }
